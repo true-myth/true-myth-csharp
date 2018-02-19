52d5331ae721e83336583c66d90bc767e105e4e0
Chris Riesgo <cmriesgo@gmail.com>
2018-02-19 10:36:46 -0600

    Finished a straightforward Result implementation
    - Finished implementing the known IResult Ok/Err implementaions
    - Back-filled the `Result` static class with several helper methods
    - Implemented IEquatable<T> on Ok/Err to make equality comparisons easier

--- src/TrueMyth/Result.cs
+++ src/TrueMyth/Result.cs
@@ -2,7 +2,7 @@
 
 namespace TrueMyth
 {
-    public interface IResult<TValue, TError>
+    public interface IResult<out TValue, TError>
     {
         TValue UnsafelyUnwrap();
         TError UnsafelyUnwrapErr();
@@ -20,33 +20,27 @@ namespace TrueMyth
         public static bool IsOk<TValue, TError>(IResult<TValue, TError> result) => result.IsOk();
         public static bool IsErr<TValue, TError>(IResult<TValue, TError> result) => result.IsErr();
 
-        public static IResult<TSelected, TError> Select<TValue, TSelected, TError>(
-            Func<TValue, TSelected> selector,
-            IResult<TValue, TError> result
-        ) => IsOk(result)
-            ? new Ok<TSelected, TError>(selector(UnsafelyUnwrap(result)))
-            : result as IResult<TSelected, TError>;
+        public static IResult<TSelected, TError> Select<TValue, TSelected, TError>(Func<TValue, TSelected> selector, IResult<TValue, TError> result) 
+            => IsOk(result)
+                ? new Ok<TSelected, TError>(selector(UnsafelyUnwrap(result)))
+                : result as IResult<TSelected, TError>;
 
-        public static IResult<TSelected, TError> SelectMany<TValue, TSelected, TError>(
-            Func<TValue, IResult<TSelected, TError>> selector,
-            IResult<TValue, TError> result
-        ) => IsOk(result)
-            ? selector(UnsafelyUnwrap(result))
-            : result as IResult<TSelected, TError>;
+        public static IResult<TSelected, TError> SelectMany<TValue, TSelected, TError>(Func<TValue, IResult<TSelected, TError>> selector, IResult<TValue, TError> result) 
+            => IsOk(result)
+                ? selector(UnsafelyUnwrap(result))
+                : result as IResult<TSelected, TError>;
 
-        public static IResult<TValue, TSelectedError> SelectErr<TValue, TError, TSelectedError>(
-            Func<TError, TSelectedError> selector,
-            IResult<TValue, TError> result
-        ) => IsErr(result)
-            ? new Err<TValue, TSelectedError>(selector(UnsafelyUnwrapErr(result)))
-            : result as IResult<TValue, TSelectedError>;
+        public static IResult<TValue, TSelectedError> SelectErr<TValue, TError, TSelectedError>(Func<TError, TSelectedError> selector, IResult<TValue, TError> result) 
+            => IsErr(result)
+                ? new Err<TValue, TSelectedError>(selector(UnsafelyUnwrapErr(result)))
+                : result as IResult<TValue, TSelectedError>;
 
         public static TValue UnsafelyUnwrap<TValue, TError>(IResult<TValue, TError> result) => result.UnsafelyUnwrap();
         public static TError UnsafelyUnwrapErr<TValue, TError>(IResult<TValue, TError> result) =>
             result.UnsafelyUnwrapErr();
     }
 
-    public class Ok<TValue, TError> : IResult<TValue, TError>
+    public class Ok<TValue, TError> : IResult<TValue, TError>, IEquatable<IResult<TValue, TError>>
     {
         private readonly TValue _value;
 
@@ -61,24 +55,25 @@ namespace TrueMyth
         public bool IsOk() => true;
         public bool IsErr() => false;
 
-        public IResult<TMapped, TError> Select<TMapped>(Func<TValue, TMapped> selector) =>
-            Result.Select(selector, this);
+        public IResult<TMapped, TError> Select<TMapped>(Func<TValue, TMapped> selector) 
+            => Result.Select(selector, this);
 
-        public IResult<TSelected, TError> SelectMany<TSelected>(Func<TValue, IResult<TSelected, TError>> selector) =>
-            Result.SelectMany(selector, this);
+        public IResult<TSelected, TError> SelectMany<TSelected>(Func<TValue, IResult<TSelected, TError>> selector) 
+            => Result.SelectMany(selector, this);
 
-        public IResult<TValue, TSelectedError> SelectErr<TSelectedError>(Func<TError, TSelectedError> selector) =>
-            Result.SelectErr(selector, this);
+        public IResult<TValue, TSelectedError> SelectErr<TSelectedError>(Func<TError, TSelectedError> selector) 
+            => Result.SelectErr(selector, this);
 
         public TValue UnsafelyUnwrap() => _value;
 
         public TError UnsafelyUnwrapErr()
-        {
-            throw new Exception("Tried to `UnsafelyUnwrapError with Ok`");
-        }
+            => throw new Exception("Tried to `UnsafelyUnwrapError with Ok`");
+
+        public bool Equals(IResult<TValue, TError> other)
+            => other.IsOk() && UnsafelyUnwrap().Equals(other.UnsafelyUnwrap());
     }
 
-    public class Err<TValue, TError> : IResult<TValue, TError>
+    public class Err<TValue, TError> : IResult<TValue, TError>, IEquatable<IResult<TValue, TError>>
     {
         private readonly TError _error;
 
@@ -93,20 +88,21 @@ namespace TrueMyth
         public bool IsOk() => false;
         public bool IsErr() => true;
 
-        public IResult<TMapped, TError> Select<TMapped>(Func<TValue, TMapped> selector) =>
-            Result.Select(selector, this);
+        public IResult<TMapped, TError> Select<TMapped>(Func<TValue, TMapped> selector) 
+            => Result.Select(selector, this);
 
-        public IResult<TSelected, TError> SelectMany<TSelected>(Func<TValue, IResult<TSelected, TError>> selector) =>
-            Result.SelectMany(selector, this);
+        public IResult<TSelected, TError> SelectMany<TSelected>(Func<TValue, IResult<TSelected, TError>> selector) 
+            => Result.SelectMany(selector, this);
 
-        public IResult<TValue, TSelectedError> SelectErr<TSelectedError>(Func<TError, TSelectedError> selector) =>
-            Result.SelectErr(selector, this);
+        public IResult<TValue, TSelectedError> SelectErr<TSelectedError>(Func<TError, TSelectedError> selector) 
+            => Result.SelectErr(selector, this);
 
         public TValue UnsafelyUnwrap()
-        {
-            throw new Exception("Tried to `UnsafelyUnwrapErr` with an `Error``");
-        }
+            => throw new Exception("Tried to `UnsafelyUnwrapErr` with an `Error``");
 
         public TError UnsafelyUnwrapErr() => _error;
+
+        public bool Equals(IResult<TValue, TError> other)
+            => other.IsErr() && UnsafelyUnwrapErr().Equals(other.UnsafelyUnwrapErr());
     }
 }
