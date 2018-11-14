604c2d640f82a78499b86b601199b44e7860aeb3
Benjamin Collins <aggieben@gmail.com>
2018-11-14 15:17:45 -0600

    Add new tests

--- test/TrueMyth.Test/ResultTests.cs
+++ test/TrueMyth.Test/ResultTests.cs
@@ -6,7 +6,7 @@ namespace TrueMyth.Test
 {
     using SimpleResult = Result<int, string>;
 
-    public class ResultTests
+    public partial class ResultTests
     {
         [Fact]
         public void Implicit_Ok_OK()
@@ -117,6 +117,95 @@ namespace TrueMyth.Test
             Assert.True(result.IsOk);
             Assert.False(result.IsErr);
             Assert.Throws<InvalidOperationException>(() => result.UnsafelyUnwrapErr());
+        }
+
+        [Fact]
+        public void And_OkReturnsAndResult_Ok()
+        {
+            // arrange
+            var result = SimpleResult.Ok(7);
+            var andResult = SimpleResult.Ok(1);
+
+            // act
+            var r = result.And(andResult);
+
+            // assert
+            Assert.Equal(andResult, r);
+        }
+
+        // Match
+
+        // Or
+
+        // OrElse
+
+        // constructor (validate that no invalid states can be created)
+
+        // Select
+
+        // SelectErr
+
+        // SelectOk
+
+        // SelectOrDefault
+
+        // SelectOrElse
+
+        // ToString
+
+        // UnwrapOr
+
+        // UnwrapOrElse        
+
+        [Theory]
+        [MemberData(nameof(GetEqualPairs))]
+        public void Equals_Same_True(SimpleResult r1, SimpleResult r2)
+        {
+            // arrange
+            // act
+            var equalityResult = r1.Equals(r2);
+
+            // assert
+            Assert.True(equalityResult);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetUnequalPairs))]
+        public void Equals_Different_False(SimpleResult r1, SimpleResult r2)
+        {
+            // arrange - in theory
+            // act
+            var equalityResult = r1.Equals(r2);
+
+            // assert
+            Assert.False(equalityResult);
+        }
+
+        // GetHashCode
+        [Theory]
+        [MemberData(nameof(GetEqualPairs))]
+        public void GetHashCode_SameResults_Equal(SimpleResult r1, SimpleResult r2)
+        {
+            // arrange - in theory
+            // act
+            var hc1 = r1.GetHashCode();
+            var hc2 = r2.GetHashCode();
+
+            // assert
+            Assert.Equal(hc1, hc2);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetUnequalPairs))]
+        public void GetHashCode_DifferentResult_NotEqual(SimpleResult r1, SimpleResult r2)
+        {
+            // arrange - in theory
+            // act
+            var hc1 = r1.GetHashCode();
+            var hc2 = r2.GetHashCode();
+
+            // assert
+            Assert.NotEqual(hc1, hc2);
         }
     }
 }
